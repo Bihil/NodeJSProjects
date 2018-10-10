b0f3bd5e26e2d4d94cf5de25cffa2bb74a7fb4eb
Bihil <bihilkon@gmail.com>
2018-10-10 15:25:46 +0200

    added emailing functionality

--- expressWebsite/app.js
+++ expressWebsite/app.js
@@ -9,14 +9,51 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__filename, 'public')));
+app.use(bodyParser.urlencoded({
+ extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
- res.render('index', {title: 'Welcome'});
+app.get('/', function (req, res) {
+ res.render('index', {
+  title: 'Computer Not Working?'
+ });
 });
-app.get('/about', function(req, res){
+
+app.get('/about', function (req, res) {
  res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+ res.render('contact');
+});
+
+app.post('/contact/send', function (req, res) {
+ var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+   user: 'gmail@gmail.com',
+   pass: 'password'
+  }
+ });
+
+ var mailOptions = {
+  from: 'Brad Traversy <techguyinfo@gmail.com>',
+  to: 'support@joomdigi.com',
+  subject: 'Website Submission',
+  text: 'You have a submission with the following details... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
+  html: '<p>You have a submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+ };
+
+ transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+   console.log(error);
+   res.redirect('/');
+  } else {
+   console.log('Message Sent: ' + info.response);
+   res.redirect('/');
+  }
+ });
 });
 
 app.listen(3000);
